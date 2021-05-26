080c225119af8a739dcd60bf283c637c7bbe4ada
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2021-05-26 11:56:45 +0200

    [test] Add tests for the web app

--- web/web/test_app.py
+++ web/web/test_app.py
@@ -1,4 +1,4 @@
-"""File containing unittests for the API."""
+"""File containing unittests for the APP."""
 
 from unittest import TestCase
 from unittest.mock import MagicMock
@@ -7,9 +7,9 @@ from app import app
 from blueprints.api import client, collect
 from collector_pb2 import ElectricalData
 
-class TestAPI(TestCase):
+class TestAPP(TestCase):
 
-    """Unittest for the web API."""
+    """Unittest for the web APP."""
 
     def setUp(self):
         app.config['TESTING'] = True
@@ -21,6 +21,5 @@ class TestAPI(TestCase):
         """Make sure collect calls the microservice."""
         client.Collect = MagicMock()
         client.Collect.return_value = ElectricalData(states=[])
-        response = self.app.get("/api/collect")
+        response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, [])
